6a351227df6f249b7905c0d8662df5c4c273b84f
ValeraKaktotak <valerakaktotak@gmail.com>
2023-07-31 21:11:06 +0300

    added React.Lazy

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
+import { Suspense } from 'react'
 import { Link, Route, Routes } from 'react-router-dom'
 import './index.scss'
-import AboutPage from './pages/AboutPage/AboutPage'
-import MainPage from './pages/MainPage/MainPage'
+import { AboutPageLazy } from './pages/AboutPage/AboutPage.lazy'
+import { MainPageLazy } from './pages/MainPage/MainPage.lazy'
 
 const App = () => {
 	return (
 		<div>
 			<Link to={'/'}>Main Page</Link>
 			<Link to={'/about'}>About Us</Link>
-			<Routes>
-				<Route path='/' element={<MainPage />} />
-				<Route path='/about' element={<AboutPage />} />
-			</Routes>
+			<Suspense fallback='Loading...'>
+				<Routes>
+					<Route path='/' element={<MainPageLazy />} />
+					<Route path='/about' element={<AboutPageLazy />} />
+				</Routes>
+			</Suspense>
 		</div>
 	)
 }
