5166d2577c871924aa17ac580d44f630e18ee367
ValeraKaktotak <valerakaktotak@gmail.com>
2023-08-05 17:51:58 +0300

    created my own helper classNames

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Suspense } from 'react'
 import { Link, Route, Routes } from 'react-router-dom'
+import { classNames } from './helpers/classNames/classNames'
 import { AboutPageLazy } from './pages/AboutPage/AboutPage.lazy'
 import { MainPageLazy } from './pages/MainPage/MainPage.lazy'
 import './styles/index.scss'
@@ -9,7 +10,7 @@ const App = () => {
 	const { theme, toggleTheme } = useTheme()
 
 	return (
-		<div className={`app ${theme}`}>
+		<div className={classNames('app', {}, [theme])}>
 			<button onClick={toggleTheme}>TOGGLE THEME</button>
 			<Link to={'/'}>Main Page</Link>
 			<Link to={'/about'}>About Us</Link>
