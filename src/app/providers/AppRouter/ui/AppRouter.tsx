8c03434d93e5cc0d19738e746d77b6eca37199c4
ValeraKaktotak <valerakaktotak@gmail.com>
2023-08-08 22:45:09 +0300

    added Sidebar

--- src/app/providers/AppRouter/ui/AppRouter.tsx
+++ src/app/providers/AppRouter/ui/AppRouter.tsx
@@ -7,7 +7,11 @@ export const AppRouter = () => {
 		<Suspense fallback='Loading...'>
 			<Routes>
 				{Object.values(routeConfig).map((value) => (
-					<Route key={value.path} path={value.path} element={value.element} />
+					<Route
+						key={value.path}
+						path={value.path}
+						element={<div className='page-wrapper'>{value.element}</div>}
+					/>
 				))}
 			</Routes>
 		</Suspense>
---
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -0,0 +1,27 @@
+import { FC, useState } from 'react'
+import { classNames } from 'shared/lib/classNames/classNames'
+import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher'
+import styles from './Sidebar.module.scss'
+
+interface SidebarProps {
+	className?: string
+}
+
+export const Sidebar: FC<SidebarProps> = ({ className }) => {
+	const [collapsed, setCollapsed] = useState(true)
+
+	const onToggle = () => {
+		setCollapsed((prev) => !prev)
+	}
+	return (
+		<div
+			className={classNames(styles.Sidebar, { [styles.collapsed]: collapsed }, [
+				className,
+			])}>
+			<button onClick={onToggle}>toggle</button>
+			<div className={styles.switchers}>
+				<ThemeSwitcher />
+			</div>
+		</div>
+	)
+}
