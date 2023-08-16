23fd495b998fb3c19342d70bec1c5fbfa1134fda
ValeraKaktotak <valerakaktotak@gmail.com>
2023-08-16 19:46:02 +0300

    set up storybook/webpack.config.ts

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,47 +1,32 @@
-import MiniCssExtractPlugin from 'mini-css-extract-plugin'
 import type webpack from 'webpack'
+import { buildCssLoader } from './loaders/buildCssLoader'
 import { type BuildOptions } from './types/config'
 /* в rules конфигурируем лодеры они предназначены для обработки файлов выходящих за рамки js(png, css, ts и т.д.) */
 
 export const builderLoaders = ({
-	isDev
+	isDev,
 }: BuildOptions): webpack.RuleSetRule[] => {
 	// порядок лодеров имеет значение, поэтому вносим их в переменную в опр. порядке
 	const svgLoader = {
 		test: /\.svg$/i,
 		issuer: /\.[jt]sx?$/,
-		use: [{ loader: '@svgr/webpack', options: { icon: true } }]
+		use: [{ loader: '@svgr/webpack', options: { icon: true } }],
 	}
 	const fileLoader = {
 		test: /\.(png|jpe?g|gif)$/i,
 		use: [
 			{
-				loader: 'file-loader'
-			}
-		]
+				loader: 'file-loader',
+			},
+		],
 	}
 	// если не используем typescript то нужен babel
 	const typescriptLoader = {
 		test: /\.tsx?$/,
 		use: 'ts-loader',
-		exclude: /node_modules/
+		exclude: /node_modules/,
 	}
-	const cssLoader = {
-		test: /\.s[ac]ss$/i,
-		use: [
-			isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
-			{
-				loader: 'css-loader',
-				options: {
-					modules: {
-						auto: (resPath: string) =>
-							Boolean(resPath.includes('.module.scss')),
-						localIdentName: isDev ? '[path][name]__[local]' : '[hash:base64:8]'
-					}
-				}
-			},
-			'sass-loader'
-		]
-	}
+	const cssLoader = buildCssLoader(isDev)
+
 	return [typescriptLoader, cssLoader, svgLoader, fileLoader]
 }
