651f6d319b9d3a365d9699942191a666cc815bd9
ValeraKaktotak <valerakaktotak@gmail.com>
2023-08-18 22:28:12 +0300

    fix thmeheToggle in sidebar.stories

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.stories.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react'
 import { Theme } from 'app/providers/ThemeProvider'
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator'
+import { ThemeSwitcherDecorator } from 'shared/config/storybook/ThemeSwitcherDecorator/ThemeSwitcherDecorator'
 import { Sidebar } from './Sidebar'
 
 const meta = {
@@ -17,5 +18,5 @@ export const SidebarLight: Story = {
 }
 export const SidebarDark: Story = {
 	args: {},
-	decorators: [ThemeDecorator(Theme.DARK)],
+	decorators: [ThemeDecorator(Theme.DARK), ThemeSwitcherDecorator(Theme.DARK)],
 }
