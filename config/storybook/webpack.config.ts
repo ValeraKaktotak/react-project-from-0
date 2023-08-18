d1413e55c03ec8e32e30d8b2745f6356befc2b39
ValeraKaktotak <valerakaktotak@gmail.com>
2023-08-18 15:24:22 +0300

    added Sidebar in storybook and fixed svg loader

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -1,5 +1,5 @@
 import path from 'path'
-import webpack from 'webpack'
+import webpack, { RuleSetRule } from 'webpack'
 import { buildCssLoader } from '../build/loaders/buildCssLoader'
 import { BuildPath } from '../build/types/config'
 
@@ -13,6 +13,23 @@ export default ({ config }: { config: webpack.Configuration }) => {
 	config.resolve?.modules?.push(paths.src)
 	config.resolve?.extensions?.push('.ts', '.tsx')
 	config.module?.rules?.push(buildCssLoader(true))
+
+	//отключаем дефольный svg лодер сторибука
+	if (config.module && config.module.rules) {
+		// @ts-ignore
+		config.module.rules = config.module.rules.map((rule: RuleSetRule) => {
+			if (/svg/.test(rule.test as string)) {
+				return { ...rule, exclude: /\.svg$/i }
+			}
+			return rule
+		})
+	}
+
+	config.module?.rules?.push({
+		test: /\.svg$/i,
+		issuer: /\.[jt]sx?$/,
+		use: [{ loader: '@svgr/webpack', options: { icon: true } }],
+	})
 
 	return config
 }
