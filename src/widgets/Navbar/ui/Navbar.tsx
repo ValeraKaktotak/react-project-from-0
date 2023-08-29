2ce6f5d6851b3387c98b91fda99b3e94230efc4d
ValeraKaktotak <valerakaktotak@gmail.com>
2023-08-29 07:57:59 +0300

    added icons to sidebar links and styled them

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,7 +1,6 @@
 import { type FC } from 'react'
 import { useTranslation } from 'react-i18next'
 import { classNames } from 'shared/lib/classNames/classNames'
-import { AppLink, AppThemeClass } from 'shared/ui/AppLink/AppLink'
 import styles from './Navbar.module.scss'
 
 interface NavbarProps {
@@ -12,14 +11,14 @@ export const Navbar: FC<NavbarProps> = ({ className }) => {
 	const { t } = useTranslation()
 	return (
 		<div className={classNames(styles.Navbar, {}, [className])}>
-			<div className={styles.links}>
+			{/* <div className={styles.links}>
 				<AppLink theme={AppThemeClass.PRIMARY} to={'/'}>
 					{t('Main page')}
 				</AppLink>
 				<AppLink theme={AppThemeClass.PRIMARY} to={'/about'}>
 					{t('About Us')}
 				</AppLink>
-			</div>
+			</div> */}
 		</div>
 	)
 }
