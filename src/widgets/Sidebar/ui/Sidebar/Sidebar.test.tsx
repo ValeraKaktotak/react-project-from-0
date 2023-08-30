ab930c2c8735542e52ba87f888aa4ae5b48cb2cf
ValeraKaktotak <valerakaktotak@gmail.com>
2023-08-30 13:37:50 +0300

    added renderWithRoute hoc to tests

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,14 +1,15 @@
 import { fireEvent, screen } from '@testing-library/react'
-import { renderWithTranslations } from 'shared/lib/tests/renderWithTranslations/renderWithTranslations'
+import { renderWithRoutes } from 'shared/lib/tests/renderWithRoutes/renderWithRoutes'
 import { Sidebar } from 'widgets/Sidebar/ui/Sidebar/Sidebar'
 
 describe('classNames', () => {
 	test('Test render', () => {
-		renderWithTranslations(<Sidebar />)
+		renderWithRoutes(<Sidebar />)
 		expect(screen.getByTestId('sidebarTestId')).toBeInTheDocument()
 	}),
 		test('Test toggle sidebar', () => {
-			renderWithTranslations(<Sidebar />)
+			// renderWithRoutes(<Sidebar />)
+			renderWithRoutes(<Sidebar />)
 			const toggleBtn = screen.getByTestId('sidebar-toggle')
 			expect(screen.getByTestId('sidebarTestId')).toBeInTheDocument()
 			fireEvent.click(toggleBtn)
