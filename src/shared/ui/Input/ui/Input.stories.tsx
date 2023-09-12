5758c10819cb8ba2f928989c6f8e2d5e7ca183ea
ValeraKaktotak <valerakaktotak@gmail.com>
2023-09-12 16:15:32 +0300

    added input stories

---
+++ src/shared/ui/Input/ui/Input.stories.tsx
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Input } from './Input'
+
+const meta = {
+	title: 'shared/Input',
+	component: Input,
+} satisfies Meta<typeof Input>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const OutlineLight: Story = {
+	args: {
+		placeholder: 'test placeholder',
+		value: 'test text',
+	},
+}
