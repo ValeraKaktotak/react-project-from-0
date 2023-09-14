8e265335e0e6e43751d3278317101e6df184271c
ValeraKaktotak <valerakaktotak@gmail.com>
2023-09-14 14:31:47 +0300

    deleted .github folder

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,22 +1,22 @@
-import { Suspense, type FC } from 'react'
-import { classNames } from 'shared/lib/classNames/classNames'
-import { Loader } from 'shared/ui/Loader'
-import { Navbar } from 'widgets/Navbar'
-import { Sidebar } from 'widgets/Sidebar'
-import { AppRouter } from './providers/AppRouter'
+import { type FC, Suspense } from "react";
+import { classNames } from "shared/lib/classNames/classNames";
+import { Loader } from "shared/ui/Loader";
+import { Navbar } from "widgets/Navbar";
+import { Sidebar } from "widgets/Sidebar";
+import { AppRouter } from "./providers/AppRouter";
 
 const App: FC = () => {
-	return (
-		<div className={classNames('app')}>
-			<Suspense fallback={<Loader />}>
-				<Navbar />
-				<div className='app-container'>
-					<Sidebar />
-					<AppRouter />
-				</div>
-			</Suspense>
-		</div>
-	)
-}
+  return (
+    <div className={classNames("app")}>
+      <Suspense fallback={<Loader />}>
+        <Navbar />
+        <div className="app-container">
+          <Sidebar />
+          <AppRouter />
+        </div>
+      </Suspense>
+    </div>
+  );
+};
 
-export default App
+export default App;
