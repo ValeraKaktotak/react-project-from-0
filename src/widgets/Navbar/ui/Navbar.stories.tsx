e0dd9457549917eaa03092094b9f1234a8ae3a2d
ValeraKaktotak <valerakaktotak@gmail.com>
2023-09-19 21:19:33 +0300

    fixed stiries

--- src/widgets/Navbar/ui/Navbar.stories.tsx
+++ src/widgets/Navbar/ui/Navbar.stories.tsx
@@ -2,10 +2,11 @@ import type { Meta, StoryObj } from '@storybook/react'
 import { Theme } from 'app/providers/ThemeProvider'
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator'
 import { Navbar } from './Navbar'
+import { StoreDecorator } from 'shared/config/storybook/StoreDecorator/StoreDecorator'
 
 const meta = {
 	title: 'widget/Navbar',
-	component: Navbar,
+	component: Navbar
 } satisfies Meta<typeof Navbar>
 
 export default meta
@@ -14,8 +15,9 @@ type Story = StoryObj<typeof meta>
 
 export const NavbarLight: Story = {
 	args: {},
+	decorators: [StoreDecorator({})]
 }
 export const NavbarDark: Story = {
 	args: {},
-	decorators: [ThemeDecorator(Theme.DARK)],
+	decorators: [ThemeDecorator(Theme.DARK), StoreDecorator({})]
 }
